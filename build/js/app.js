ca7e8bedf1971a751fc572fc0773202c1e6994e7
Denwebart <denwebart@gmail.com>
2024-09-21 19:47:48 +0300

    new feature(main nav) Main navigation created but yet without burger menu

---
+++ build/js/app.js
@@ -0,0 +1,8 @@
+// Detecting When a Sticky Element Gets Pinned
+const el = document.querySelector('.main-menu');
+const observer = new IntersectionObserver(
+    ([e]) => e.target.classList.toggle('is-pinned', e.intersectionRatio < 1),
+    { threshold: [1] }
+);
+
+observer.observe(el);
