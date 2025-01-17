37386929dc88137281d3cd44c961d15ee1ea6105
Denwebart <denwebart@gmail.com>
2025-01-17 16:08:23 +0200

    refactoring(categories block) Categories blocks refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./build/*.html'],
+    content: ['./assets/*.html'],
     theme: {
         container: {
             center: true,
